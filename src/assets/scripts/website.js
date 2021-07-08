ddcf4c2e4c23b8d8316c704edc064c3f050be4c3
Lukhman Latheef <lukhman.latheef@gmail.com>
2021-07-08 20:24:29 +0530

    Theme added

---
+++ src/assets/scripts/website.js
@@ -0,0 +1,2 @@
+"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("/service-worker.js").then(function(a){},function(a){})}),function(){"use strict";$(document).foundation()}();
+//# sourceMappingURL=website.js.map
